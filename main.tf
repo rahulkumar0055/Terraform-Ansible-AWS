e7d0f71db9b173ff9a94689cceda693b75522699
rahulkumar0055 <rahulkumarnv332@gmail.com>
2025-07-03 16:43:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,34 +5,8 @@ terraform {
       version = ">= 2.0"
     }
   }
-  required_version = ">= 0.12"
-
-  backend "s3" {
-    bucket  = "terraform-ansible-aws-project-bucket"
-    key     = "env/dev/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
+  required_version = ">= 0.12" 
 }
-
-provider "vault" {
-  address         = var.vault_addr
-  token           = var.vault_token
-  skip_tls_verify = true
-}
-
-
-data "vault_generic_secret" "aws_creds" {
-  path = "aws-creds/myapp"
-}
-
-provider "aws" {
-  access_key = data.vault_generic_secret.aws_creds.data["access_key"]
-  secret_key = data.vault_generic_secret.aws_creds.data["secret_key"]
-  region     = var.aws_region
-}   
-
-
 module "vpc" {
   source         = "./Modules/VPC"
   vpc_cidr_block = var.vpc_cidr_block
